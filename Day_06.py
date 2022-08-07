9f45326970e564f2c2b388eb69fdae6ddf541c13
Scott <scott@linfords.com>
2022-08-07 05:21:22 -0700

    Day 6 part 1 in progress

--- Day_06.py
+++ Day_06.py
@@ -15,11 +15,35 @@ def read_puzzle_data(filename):
         return rval
 
 
+def size_the_grid(points):
+    max_x, max_y = -1, -1
+    for point in points:
+        if point[0] > max_x:
+            max_x = point[0]
+        if point[1] > max_y:
+            max_y = point[1]
+    return max(max_x, max_y)
+
+
+def create_grid(points, grid_size):
+    rval = []
+    for _ in range(grid_size):
+        rval.append(['.'] * grid_size)
+    return rval
+
+def render_grid(grid):
+    rval = []
+    for y in range(len(grid)):
+        rval.append(''.join(grid[y]))
+        rval.append('\n')
+    return ''.join(rval)
 
 
 def part_one():
     points = read_puzzle_data('Day_06_short_data.txt')
-    print(points)
+    grid_size = size_the_grid(points)
+    grid = create_grid(points, grid_size)
+    print(render_grid(grid))
 
 
 part_one()
