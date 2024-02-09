955a4f1a115d0d7cea436ad7cd53982ab20f80c8
Scott <scott@linfords.com>
2024-02-09 04:29:36 -0800

    Day 12 parsing complete

--- Day_12.py
+++ Day_12.py
@@ -1,4 +1,8 @@
+import re
 import unittest
+from collections import defaultdict
+from dataclasses import dataclass
+
 
 def read_puzzle_input(filename):
     with open(filename, 'r') as f:
@@ -6,13 +10,36 @@ def read_puzzle_input(filename):
     return data
 
 
-def parse_data(data: str):
-    return data
+@dataclass
+class Rule:
+    pattern: str
+    result: str
 
+
+def parse_data(data: str):
+    data = data.split('\n\n')
+    result = re.search(r'initial state: (.*)', data[0])
+    state = result.group(1)
+    data = data[1].split('\n')
+    rules = []
+    for row in data:
+        result = re.search(r'(.....) => (.)', row)
+        rules.append(Rule(result.group(1), result.group(2)))
+
+    return initialize_state(state), rules
+
+def initialize_state(string):
+    d = defaultdict(lambda: '.')
+    for i, c in enumerate(string):
+        if c == '#':
+            # noinspection PyTypeChecker
+            d[i] = '#'
+    return d
 
 def part_one(filename):
     data = read_puzzle_input(filename)
-    data = parse_data(data)
+    state, rules = parse_data(data)
+
     return -1
 
 
@@ -24,7 +51,7 @@ def part_two(filename):
 
 class Test(unittest.TestCase):
     def test_part_one(self):
-        self.assertEqual(-1, part_one('Day_12_data.txt'))
+        # self.assertEqual(-1, part_one('Day_12_data.txt'))
         self.assertEqual(-1, part_one('Day_12_short_data.txt'))
 
     def test_part_two(self):
