e1e4526e7093cfe5e6486b72d2ff0c97996c4484
Scott <scott@linfords.com>
2024-02-09 13:05:44 -0800

    Day 13 parsing complete

--- Day_13.py
+++ Day_13.py
@@ -1,4 +1,6 @@
 import unittest
+from dataclasses import dataclass
+from enum import Enum, auto
 
 
 def read_puzzle_input(filename):
@@ -7,16 +9,56 @@ def read_puzzle_input(filename):
     return data
 
 
+class Heading(Enum):
+    UP = auto()
+    DOWN = auto()
+    LEFT = auto()
+    RIGHT = auto()
+
+@dataclass
+class Cart:
+    x: int
+    y: int
+    heading: Heading
+
+
+def scan_for_carts(grid):
+    carts = []
+    height = len(grid)
+    width = len(grid[0])
+    for y in range(height):
+        for x in range(width):
+            match grid[y][x]:
+                case '^':
+                    heading = Heading.UP
+                case 'v':
+                    heading = Heading.DOWN
+                case '<':
+                    heading = Heading.LEFT
+                case '>':
+                    heading = Heading.RIGHT
+                case _:
+                    heading = None
+            if heading:
+                carts.append(Cart(x, y, heading))
+    return carts
 
 
 def parse_data(data):
-    data = data.split('\n')
-    return data
+    grid = [list(a) for a in data.split('\n')]
+    longest = 0
+    for row in grid:
+        longest = max(longest, len(row))
+    for row in grid:
+        while len(row) < longest:
+            row.append(' ')
+    carts = scan_for_carts(grid)
+    return grid, carts
 
 
 def part_one(filename):
     data = read_puzzle_input(filename)
-    data = parse_data(data)
+    grid, carts = parse_data(data)
     return -1
 
 
@@ -28,7 +70,7 @@ def part_two(filename):
 
 class Test(unittest.TestCase):
     def test_part_one(self):
-        self.assertEqual(-1, part_one('Day_13_data.txt'))
+        # self.assertEqual(-1, part_one('Day_13_data.txt'))
         self.assertEqual(-1, part_one('Day_13_short_data.txt'))
 
     def test_part_two(self):
