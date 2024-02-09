b330a9df5884891a5140b2f8d35b40d072ea6c99
Scott <scott@linfords.com>
2024-02-08 22:33:15 -0800

    Day 11 stubbed

---
+++ Day_11.py
@@ -0,0 +1,18 @@
+import unittest
+
+def part_one(serial_number):
+    return -1
+
+
+def part_two():
+    return -1
+
+
+class Test(unittest.TestCase):
+    def test_part_one(self):
+        # self.assertEqual(-1, part_one(1955))
+        self.assertEqual((33, 45), part_one(18))
+
+    def test_part_two(self):
+        self.assertEqual(-1, part_two())
+        self.assertEqual(-1, part_two())
