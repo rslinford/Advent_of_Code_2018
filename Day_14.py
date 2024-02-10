87b6c89b19b4fe462c7d38c2a1752358b4352851
Scott <scott@linfords.com>
2024-02-10 13:58:20 -0800

    Day 13 part one passes, part two takes forever

--- Day_14.py
+++ Day_14.py
@@ -12,19 +12,22 @@ class ScoreBoard:
                 return j
         return None
 
+    def advance_elves(self):
+        for i, elf in enumerate(self.elves):
+            self.elves[i] = (elf + 1 + self.scores[elf]) % len(self.scores)
+
     def __repr__(self):
         string = []
         for i, score in enumerate(self.scores):
             elf_id = self._elf_id_at(i)
             if elf_id is None:
-                string.append(str(score))
+                string.append(f'{score}')
             elif elf_id == 0:
                 string.append(f'({score})')
             elif elf_id == 1:
                 string.append(f'[{score}]')
             else:
                 string.append(f'{score}')
-
         return ', '.join(string)
 
     def create_new_recipes(self):
@@ -34,22 +37,47 @@ class ScoreBoard:
         for c in str(total):
             self.scores.append(int(c))
 
+    def score_ten(self, puzzle_input):
+        string = []
+        for i in range(puzzle_input, puzzle_input + 10):
+            string.append(f'{self.scores[i]}')
+        return int(''.join(string))
+
+    def last_five(self):
+        if len(self.scores) < 5:
+            return ''
+        string = []
+        for score in self.scores[-5:]:
+            string.append(f'{score}')
+        return ''.join(string)
 
 def part_one(puzzle_input):
     sb = ScoreBoard()
-    sb.create_new_recipes()
-    return -1
+    # print(sb)
+    while len(sb.scores) < puzzle_input + 10:
+        sb.create_new_recipes()
+        sb.advance_elves()
+        # print(sb)
+    return sb.score_ten(puzzle_input)
 
 
 def part_two(puzzle_input):
-    return -1
+    sb = ScoreBoard()
+    last_five = ''
+    while last_five != puzzle_input:
+        sb.create_new_recipes()
+        sb.advance_elves()
+        last_five = sb.last_five()
+    return len(sb.scores) - 5
 
 
 class Test(unittest.TestCase):
     def test_part_one(self):
         # self.assertEqual(-1, part_one(607331))
         self.assertEqual(5158916779, part_one(9))
+        self.assertEqual(9251071085, part_one(18))
+        self.assertEqual(5941429882, part_one(2018))
 
     def test_part_two(self):
-        self.assertEqual(-1, part_two(-1))
-        self.assertEqual(-1, part_two(-1))
+        self.assertEqual(-1, part_two('607331'))
+        # self.ass/tEqual(2018, part_two('59414'))
