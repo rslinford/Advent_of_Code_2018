8059d60221b20df91e44be933126a865b02d70d3
Scott <scott@linfords.com>
2024-02-10 17:59:35 -0800

    Day 15 parsing complete

--- Day_15.py
+++ Day_15.py
@@ -8,12 +8,31 @@ def read_puzzle_input(filename):
 
 
 def parse_data(data):
-    return data.split('\n')
+    return data.splitlines()
+
+
+class Grid(dict):
+    def __init__(self, lines):
+        super().__init__()
+        self.height = len(lines)
+        self.width = len(lines[0])
+        for y, line in enumerate(lines):
+            for x, c in enumerate(line):
+                self[(x, y)] = c
+
+    def print(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                print(self[(x,y)], end='')
+            print()
 
 
 def part_one(filename):
     data = read_puzzle_input(filename)
     data = parse_data(data)
+    g = Grid(data)
+    g.print()
+
     return -1
 
 
@@ -25,7 +44,7 @@ def part_two(filename):
 
 class Test(unittest.TestCase):
     def test_part_one(self):
-        self.assertEqual(-1, part_one('Day_15_data.txt'))
+        # self.assertEqual(-1, part_one('Day_15_data.txt'))
         self.assertEqual(-1, part_one('Day_15_short_data.txt'))
 
     def test_part_two(self):
